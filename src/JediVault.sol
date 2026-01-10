91016eb88b6bd6970bf871117dca2a8030f473e1
“Kyle <KyleMcodes@gmail.com>
2026-01-10 09:24:36 -0800

    ERC20 manually coded

--- src/JediVault.sol
+++ src/JediVault.sol
@@ -1,26 +1,86 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+contract JediToken {
+    string public name = "Jedi Token";
+    string public symbol = "JEDI";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
 
-contract JediVault is ERC20 {
-    using SafeERC20 for IERC20;
-    IERC20 public immutable ASSET;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
-    constructor(address _assetAddress) ERC20("Jedi Token", "JEDI") {
-        ASSET = IERC20(_assetAddress);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor (uint256 _initialSupply) {
+        totalSupply = _initialSupply;
+        balanceOf[msg.sender] = _initialSupply;
+        emit Transfer(address(0), msg.sender, _initialSupply);
+    }
+
+    function transfer(address recipient, uint256 amount) public returns (bool succcess) {
+        require(balanceOf[msg.sender] >= amount, "Insufficient balance.");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) public returns (bool success) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function TransferFrom(address sender, address recipient, uint256 amount) public returns (bool success) {
+        require(balanceOf[sender] >= amount, "Insufficient balance.");
+        require(allowance[sender][msg.sender] >= amount, "Allowance exceeded.");
+        balanceOf[sender] -= amount;
+        balanceOf[recipient] += amount;
+        allowance[sender][msg.sender] -= amount;
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+
+    function _mint(address account, uint256 amount) internal {
+        require(account != address(0), "ERC20: mint to the zero address");
+        totalSupply += amount;
+        balanceOf[account] += amount;
+        emit Transfer(address(0), account, amount);
+    }
+
+    function _burn(address account, uint256 amount) internal {
+        require(account != address (0), "ERC20: burn from the zero address");
+        require(balanceOf[account] >= amount, "Burn amount exceeds balance");
+        balanceOf[account] -= amount;
+        totalSupply -= amount;
+        emit Transfer(account, address(0), amount);
+    }
+}
+
+interface IERC20 {
+    function transfer(address recipient, uint256 amount) external returns (bool success);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool success);
+}
+
+
+
+contract JediVault is JediToken{
+    IERC20 public immutable token;
+
+    constructor(address _tokenAddress) JediToken(0) {
+        token = IERC20(_tokenAddress);
     }
 
     function deposit(uint256 amount) external {
-        ASSET.safeTransferFrom(msg.sender, address(this), amount);
+        token.transferFrom(msg.sender, address(this), amount);
         _mint(msg.sender, amount);
     }
 
     function withdraw(uint256 amount) external {
         _burn(msg.sender, amount);
-        ASSET.safeTransfer(msg.sender, amount);
+        token.transfer(msg.sender, amount);
     }
 
 }
